45d3612d91c6d145ff8503eb3ce25bf3fc8ee23c
Soumyajit paul <soumyajp@andrew.cmu.edu>
2017-01-24 15:03:50 -0500

    push2

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# Imports
+import os
+import jinja2
+import webapp2
+import logging
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -5,12 +16,10 @@ app.config['DEBUG'] = True
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.route('/')
 def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    return template.render()
 
 @app.errorhandler(404)
 def page_not_found(e):
